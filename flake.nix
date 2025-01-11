71d5f0c1b71cf4744b7057d824cdcc2e42e6aefc
Victor Borzov <borzov.vk@protonmail.com>
2025-01-11 01:36:58 +0100

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "C playgroung project with nix";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default =
+          let
+            binName = "gp";
+          in
+          pkgs.stdenv.mkDerivation {
+            name = binName;
+            src = self;
+            buildInputs = with pkgs; [ ];
+            buildPhase = "make";
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ${binName} $out/bin/
+            '';
+          };
+      });
+    };
+}
